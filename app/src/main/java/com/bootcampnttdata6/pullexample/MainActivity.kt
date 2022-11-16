2b3d844ace8377b4c77acaf18a0441bef0bac1f5
EduardoDev06 <wolf1200123@gmail.com>
2022-11-16 14:53:59 -0500

    Third commit

--- app/src/main/java/com/bootcampnttdata6/pullexample/MainActivity.kt
+++ app/src/main/java/com/bootcampnttdata6/pullexample/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //This is a comment
+        //This is a comment inside develop branch
     }
 }
